82145cb8d15ec32b6213b5db0782da9d462143d6
Sergey Yevsyukov <huskyserge@gmail.com>
2013-05-20 00:22:02 +0300

    fixing createDataBase(all good to go), changed default entity population, changed resources structure.

---
+++ src/main/resources/dataBase/createDB.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS server_monitoring_db CHARACTER SET utf8 COLLATE utf8_bin;
+GRANT ALL PRIVILEGES ON server_monitoring_db.* TO 'root'@localhost IDENTIFIED BY '' ;
+
+USE server_monitoring_db;
+
+CREATE TABLE IF NOT EXISTS employee_entity (
+id INT(50) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+employee_name VARCHAR(20) NOT NULL,
+login VARCHAR(20) NOT NULL,
+password VARCHAR(64) NOT NULL,
+email VARCHAR(50) NOT NULL,
+created TIMESTAMP,
+lastLogin TIMESTAMP,
+active INT NOT NULL,
+admin INT NOT NULL,
+INDEX(login),
+UNIQUE (id)
+) engine=InnoDB;
+
+CREATE TABLE IF NOT EXISTS server_entity (
+id INT(50) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+server_name VARCHAR(20) NOT NULL,
+address VARCHAR(50) NOT NULL,
+port INT(16) NOT NULL,
+url VARCHAR(255) NOT NULL,
+state VARCHAR(255),
+response VARCHAR(255),
+created TIMESTAMP,
+lastCheck TIMESTAMP,
+active INT NOT NULL,
+INDEX(server_name),
+UNIQUE (id)
+) engine=InnoDB;
---
+++ src/main/resources/dataBase/populateDB.sql
@@ -0,0 +1,13 @@
+INSERT INTO `employee_entity`(`id`, `employee_name`, `login`, `password`, `email`, `created`,
+`lastLogin`, `active`, `admin`) VALUES (01, 'Admin Adminov', 'admin',
+'8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin_mail@mail.com',
+'2013-05-19 00:00:01', '2038-01-09 03:14:07', 1, 1);
+INSERT INTO `employee_entity`(`id`, `employee_name`, `login`, `password`, `email`, `created`,
+`lastLogin`, `active`, `admin`) VALUES (02, 'User Userov', 'user',
+'04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb', 'user_mail@mail.com',
+'2013-05-19 00:00:01', '2038-01-09 03:14:07', 1, 0);
+
+INSERT INTO `server_entity`(`id`, `server_name`, `address`, `port`, `url`, `state`, `response`,
+ `created`, `lastCheck`, `active`) VALUES (011, 'localhost', '255.255.255.0', 8080,
+ 'http://localhost/', 'OK', 'OK', '2013-05-19 00:00:01', '2038-01-09 03:14:07', 1);
+
